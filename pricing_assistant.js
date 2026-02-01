c351f55d2c7a1bf231df166ed238b5f86ae3b162
John-Lopez-1122 <jlopez354@usf.edu>
2026-02-01 09:58:27 -0500

    Set standard variables for the Applejack Cider company

---
+++ pricing_assistant.js
@@ -0,0 +1,10 @@
+// Variables
+let productName = "Applejack Cider"
+let costPerUnit = 10
+let basePrice = 15
+let discountRate = .15
+const salesTaxRate = .07
+const fixedMonthlyCost = 2923
+
+// Pricing/cost metrics
+
