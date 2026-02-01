92755a0b18a42792a8081a1044353a0ddf53cfad
John-Lopez-1122 <jlopez354@usf.edu>
2026-02-01 10:04:13 -0500

    Pricing metrics set, time for console testing

--- pricing_assistant.js
+++ pricing_assistant.js
@@ -7,4 +7,10 @@ const salesTaxRate = .07
 const fixedMonthlyCost = 2923
 
 // Pricing/cost metrics
+let discountedPrice = basePrice * (1 - discountRate)
+let finalPriceWithTax = discountedPrice * (1 - salesTaxRate)
+let profitPerUnit = finalPriceWithTax -costPerUnit
+let breakEvenUnits = math.ciel(fixedMonthlyCost / profitPerUnit)
+let isProfitablePerUnit = profitPerUnit > 0
 
+//END: console test
